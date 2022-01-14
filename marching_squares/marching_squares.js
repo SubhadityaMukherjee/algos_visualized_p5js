ee0b9a6c9695121a00a4898d211e6c19d250e858
SubhadityaMukherjee <msubhaditya@gmail.com>
2022-01-14 12:45:19 +0100

    marching_squares

---
+++ marching_squares/marching_squares.js
@@ -0,0 +1,111 @@
+let field;
+let rez = 40;
+let cols = 0;
+let rows = 0;
+let ler = 1;
+function setup(){
+    createCanvas(1280, 720);
+    cols=1 + width / rez;
+    rows=1 + height / rez;
+    field=processing2jsNewNDimArray([cols, rows]);
+    for(let i = 0;i < cols;i++) {
+        for(let j = 0;j < rows;j++) {
+            field[i][j]=floor(random(2));
+        }
+    }
+}
+
+function linePVectorPVector(v1, v2){
+    line(v1.x, v1.y, v2.x, v2.y);
+}
+
+function draw(){
+    background(0);
+    for(let i = 0;i < cols;i++) {
+        for(let j = 0;j < rows;j++) {
+            stroke(field[i][j] * 255);
+            strokeWeight(rez * .4);
+            point(i * rez, j * rez);
+        }
+    }
+    for(let i = 0;i < cols - 1;i++) {
+        for(let j = 0;j < rows - 1;j++) {
+            let x = i * rez;
+            let y = j * rez;
+            let p = new p5.Vector(x, y);
+            let q = new p5.Vector(x + rez, y);
+            let r = new p5.Vector(x + rez, y + rez);
+            let s = new p5.Vector(x, y + rez);
+            let a = new p5.Vector();
+            let b = new p5.Vector();
+            let c = new p5.Vector();
+            let d = new p5.Vector();
+            if(ler == 1) {
+                a=p5.Vector.lerp(p, q, 0.5);
+                b=p5.Vector.lerp(q, r, 0.5);
+                c=p5.Vector.lerp(r, s, 0.5);
+                d=p5.Vector.lerp(s, p, 0.5);
+            }
+    else {
+                a=new p5.Vector(x + rez * 0.5, y);
+                b=new p5.Vector(x + rez, y + rez * 0.5);
+                c=new p5.Vector(x + rez * 0.5, y + rez);
+                d=new p5.Vector(x, y + rez * 0.5);
+            }
+            let state = getState(ceil(field[i][j]), ceil(field[i + 1][j]), ceil(field[i + 1][j + 1]), ceil(field[i][j + 1]));
+            stroke(255);
+            strokeWeight(1);
+            switch(state){
+                case 1:linePVectorPVector(c, d);
+                break;
+                case 2:linePVectorPVector(b, c);
+                break;
+                case 3:linePVectorPVector(b, d);
+                break;
+                case 4:linePVectorPVector(a, b);
+                break;
+                case 5:linePVectorPVector(a, d);
+                linePVectorPVector(b, c);
+                break;
+                case 6:linePVectorPVector(a, c);
+                break;
+                case 7:linePVectorPVector(a, d);
+                break;
+                case 8:linePVectorPVector(a, d);
+                break;
+                case 9:linePVectorPVector(a, c);
+                break;
+                case 10:linePVectorPVector(a, b);
+                linePVectorPVector(c, d);
+                break;
+                case 11:linePVectorPVector(a, b);
+                break;
+                case 12:linePVectorPVector(b, d);
+                break;
+                case 13:linePVectorPVector(b, c);
+                break;
+                case 14:linePVectorPVector(c, d);
+                break;
+            }
+        }
+    }
+}
+
+function getState(a, b, c, d){
+    return a * 8 + b * 4 + c * 2 + d * 1;
+}
+
+
+function processing2jsNewNDimArray(dimensions) {
+    if (dimensions.length > 0) {
+        let dim = dimensions[0];
+        let rest = dimensions.slice(1);
+        let newArray = new Array();
+        for (var i = 0; i < dim; i++) {
+            newArray[i] = processing2jsNewNDimArray(rest);
+        }
+        return newArray;
+     } else {
+        return undefined;
+     }
+ }
---
+++ marching_squares/marching_squares.pde
@@ -0,0 +1,118 @@
+// https://thecodingtrain.com/challenges/coding-in-the-cabana/005-marching-squares.html
+
+float[][] field;
+int rez = 40;
+int cols, rows;
+int ler = 1;
+void setup() {
+  size(1280, 720);
+  cols = 1 + width / rez;
+  rows = 1 + height / rez;
+  field = new float[cols][rows];
+  
+  for (int i = 0; i < cols; i++) {
+    for (int j = 0; j < rows; j++) {
+      field[i][j] = floor(random(2));
+    }
+  }
+}
+
+void line(PVector v1, PVector v2) {
+  line(v1.x, v1.y, v2.x, v2.y);
+}
+
+void draw() {
+  background(0); 
+  
+  for (int i = 0; i < cols; i++) {
+    for (int j = 0; j < rows; j++) {
+      stroke(field[i][j]*255);
+      strokeWeight(rez*.4);
+      point(i*rez, j*rez);
+    }
+  }
+
+  for (int i = 0; i < cols-1; i++) {
+    for (int j = 0; j < rows-1; j++) {
+      float x = i * rez;
+      float y = j * rez;
+      
+        PVector p = new PVector(x, y            );
+        PVector q = new PVector(x + rez, y);
+        PVector r = new PVector(x + rez, y + rez      );
+        PVector s = new PVector(x, y + rez);
+        PVector a = new PVector();
+        PVector b = new PVector();
+        PVector c = new PVector();
+        PVector d = new PVector();
+        
+      if(ler == 1){
+        a = PVector.lerp(p,q,0.5);
+        b = PVector.lerp(q,r,0.5);
+        c = PVector.lerp(r,s,0.5);
+        d = PVector.lerp(s,p,0.5);
+      }else{
+        a = new PVector(x + rez * 0.5, y            );
+        b = new PVector(x + rez, y + rez * 0.5);
+        c = new PVector(x + rez * 0.5, y + rez      );
+        d = new PVector(x, y + rez * 0.5);
+      }
+
+      int state = getState(ceil(field[i][j]), ceil(field[i+1][j]), 
+        ceil(field[i+1][j+1]), ceil(field[i][j+1]));
+        
+      stroke(255);
+      strokeWeight(1);
+      switch (state) {
+      case 1:  
+        line(c, d);
+        break;
+      case 2:  
+        line(b, c);
+        break;
+      case 3:  
+        line(b, d);
+        break;
+      case 4:  
+        line(a, b);
+        break;
+      case 5:  
+        line(a, d);
+        line(b, c);
+        break;
+      case 6:  
+        line(a, c);
+        break;
+      case 7:  
+        line(a, d);
+        break;
+      case 8:  
+        line(a, d);
+        break;
+      case 9:  
+        line(a, c);
+        break;
+      case 10: 
+        line(a, b);
+        line(c, d);
+        break;
+      case 11: 
+        line(a, b);
+        break;
+      case 12: 
+        line(b, d);
+        break;
+      case 13: 
+        line(b, c);
+        break;
+      case 14: 
+        line(c, d);
+        break;
+      }
+    }
+  }
+}
+
+int getState(int a, int b, int c, int d) {
+  return a * 8 + b * 4  + c * 2 + d * 1;
+}
