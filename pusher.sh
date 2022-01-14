4084ebc2f71bbc41600d4d5ec5323871ac484743
SubhadityaMukherjee <msubhaditya@gmail.com>
2022-01-14 13:39:16 +0100

    added_vis_script

--- pusher.sh
+++ pusher.sh
@@ -1,14 +1,18 @@
-#!/bin/bash
-python3 -c "
+#!/bin/python3
 import glob
 import os
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-n")
+ag = ap.parse_args()
+
 p = [x for x in glob.glob('**/*', recursive=True) if 'index.html' in x]
 for i in p:
-        main_dir = i.split('/')[0]
-        os.system(f'brave-browser --headless --screenshot='{main_dir}/image.png' '{main_dir}/index.html'')
-        with open(f'{main_dir}/README.md', 'w+') as f:
-                f.write('![im](./image.png)')
-"
-if [[ ! -z $1 ]]; then
-        git add . && git commit -m $1 && git push
-fi
+    main_dir = i.split('/')[0]
+    os.system(f'brave-browser --headless --screenshot="{main_dir}/image.png" "{main_dir}/index.html"')
+    with open(f'{main_dir}/README.md', 'w+') as f:
+            f.write('![im](./image.png)')
+
+if ag.n!=None:
+    os.system(f"git add . && git commit -m {ag.n} && git push")
