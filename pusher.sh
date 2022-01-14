90d2b6f8ffc51ee4da56f7f85849d1adcb4b5639
SubhadityaMukherjee <msubhaditya@gmail.com>
2022-01-14 13:35:30 +0100

    added_vis

--- pusher.sh
+++ pusher.sh
@@ -5,7 +5,7 @@ import os
 p = [x for x in glob.glob('**/*', recursive=True) if 'index.html' in x]
 for i in p:
         main_dir = i.split('/')[0]
-        os.system(f'brave-browser --screenshot={main_dir}/image.png {main_dir}/index.html')
+        os.system(f'brave-browser --headless --screenshot='{main_dir}/image.png' '{main_dir}/index.html'')
         with open(f'{main_dir}/README.md', 'w+') as f:
                 f.write('![im](./image.png)')
 "
