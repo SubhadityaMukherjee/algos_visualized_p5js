31ecd577fcff935313cd144a5205c69aede3a130
SubhadityaMukherjee <msubhaditya@gmail.com>
2022-01-14 12:48:33 +0100

    marching_squares

---
+++ README.md
@@ -0,0 +1,8 @@
+# Visuals
+
+- Many computer vision and computer algorithms visualized
+- Language : Processing / P5js
+        - Javascript at the back
+
+## How do I see them?
+- Just open index.html
--- pusher.sh
+++ pusher.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-find . -name "index.html" -exec bash -c 'mv "{}" "$(dirname "{}")"/README.md' \;
+#find . -name "index.html" -exec bash -c 'mv "{}" "$(dirname "{}")"/README.md' \;
 if [[ ! -z $1 ]]; then
         git add . && git commit -m $1 && git push
 fi
